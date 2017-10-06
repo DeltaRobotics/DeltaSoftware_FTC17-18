49ca5659b10a422dde00c9f7034b748306cb606c
LauraAEvans <elzagirl10@gmail.com>
2017-10-05 20:36:25 -0500

    -Added motorTest and servoTest opModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by User on 10/5/2017.
+ */
+
+@TeleOp (name = "ServoTest", group = "")
+public class ServoTest extends LinearOpMode
+{
+
+    Servo servo;
+
+    boolean leftDPadState = false;
+    boolean rightDPadState = false;
+
+    double servoPosition = 0.5;
+
+    @Override
+    public void runOpMode()
+    {
+        servo = hardwareMap.servo.get("servo");
+
+        servo.setPosition(servoPosition);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            if(gamepad1.dpad_left && !leftDPadState)
+            {
+                leftDPadState = true;
+                servoPosition -= 0.05;
+            }
+
+            else if(!gamepad1.dpad_left)
+            {
+                leftDPadState = false;
+            }
+
+            if(gamepad1.dpad_right && !rightDPadState)
+            {
+                rightDPadState = true;
+                servoPosition += 0.05;
+            }
+
+            else if(!gamepad1.dpad_right)
+            {
+                rightDPadState = false;
+            }
+
+            if(servoPosition > 1.0)
+            {
+                servoPosition = 1.0;
+            }
+
+            if(servoPosition < 0.0)
+            {
+                servoPosition = 0.0;
+            }
+
+            servo.setPosition(servoPosition);
+
+            telemetry.addData("servo Pos", servo.getPosition());
+            telemetry.addData("servoPosition Var", servoPosition);
+            telemetry.update();
+        }
+
+    }
+}
