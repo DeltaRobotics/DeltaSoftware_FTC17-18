3df95bbd760db17d44fb37088e9c25a4f23af24f
LauraAEvans <elzagirl10@gmail.com>
2017-10-19 20:55:48 -0500

    Created new DRTeleOp. Added servos and motors for arm movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRTeleOp.java
@@ -14,16 +14,24 @@ public class DRTeleOp extends LinearOpMode
     RobotHardware curiosity = new RobotHardware();
 
 
-    double slapperInit = 0.0;
+    double slapperInit = 0.5;
     double slapperPosition = 0.0;
-    double flapperInit = 0.0;
+    double flapperInit = 1.0;
     double flapperPosition = 0.0;
+
+    double armServoAdjustment = 0.0;
+    double joint1MaxSpeed = 0.40;
+
+    double joint2Position = 0.0;
+    double joint3Position = 0.0;
+
+    long initTime;
+    long changeTime = 500;
 
 
     public void runOpMode()
     {
         curiosity.init(hardwareMap);
-
 
         //Sets the end behavior of the motors - brake implies the robot will physically attempt to stop instead of just coasting
         curiosity.motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -34,6 +42,12 @@ public class DRTeleOp extends LinearOpMode
         curiosity.slapper.setPosition(slapperInit);
         curiosity.flapper.setPosition(flapperInit);
 
+        initTime = System.currentTimeMillis();
+        while((System.currentTimeMillis() - initTime) < changeTime)
+        {
+            curiosity.joint2.setPosition(0.6);
+        }
+
         waitForStart();
 
         while (opModeIsActive())
@@ -41,6 +55,8 @@ public class DRTeleOp extends LinearOpMode
             //Clipping the ranges of servos so they don't go out of bounds
             slapperPosition = Range.clip(slapperPosition, 0.05, 0.95);
             flapperPosition = Range.clip(flapperPosition, 0.05, 0.95);
+            armServoAdjustment = Range.clip(armServoAdjustment, 0.02, 0.48);
+
 
             //Setting drive motors for mecanum - gamepad 1 - driver
             curiosity.motorRF.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x) - gamepad1.right_stick_x);
@@ -67,11 +83,46 @@ public class DRTeleOp extends LinearOpMode
                 flapperPosition =- 0.05;
             }
 
+            //Manipulator - Arm controls, Joints 1 and 2
+
+            //Setting Arm Adjustment Speed
+            if(gamepad2.dpad_up)
+            {
+                armServoAdjustment += 0.02;
+            }
+            else if(gamepad2.dpad_down)
+            {
+                armServoAdjustment -= 0.02;
+            }
+
+            //Setting Joint 1
+            curiosity.joint1.setPower(gamepad2.left_stick_y * joint1MaxSpeed);
+
+            //Setting Joint 2
+            if(gamepad2.left_bumper)
+            {
+                joint2Position = (0.5 + armServoAdjustment);
+            }
+            else if(gamepad2.left_trigger > 0.4)
+            {
+                joint2Position = (0.5 - armServoAdjustment);
+            }
+            else
+            {
+                joint2Position = 0.5;
+            }
+
+            //Sending telemetry for arm data
+            telemetry.addData("armServoAdjustment", armServoAdjustment);
+            telemetry.addData("Joint 1", curiosity.joint1.getPower());
+            telemetry.addData("Joint 2", curiosity.joint2.getPosition());
+
+            telemetry.update();
+
             //Actually setting the positions of the servos
             curiosity.slapper.setPosition(slapperPosition);
             curiosity.flapper.setPosition(flapperPosition);
-
+            curiosity.joint2.setPosition(joint2Position);
         }
-
     }
 }
